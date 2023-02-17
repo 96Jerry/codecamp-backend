04c9e216c59974824ce634b66d881dd4687bb500
96jerry <junmate12@gmail.com>
2023-02-17 23:37:40 +0900

    sms-token-security-update

--- portfolio/day08/index.js
+++ portfolio/day08/index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import { Token } from "./models/token.model.js";
-import { createValidationPhone } from "./phone.js";
+import {
+  createValidationPhone,
+  sendSMS,
+  checkValidationPhone,
+} from "./phone.js";
 import bodyParser from "body-parser";
+import "dotenv/config";
 
 const Schema = mongoose.Schema;
 const app = express();
@@ -14,7 +19,6 @@ app.post("/tokens/phone", async (req, res) => {
   const number = createValidationPhone();
   // 2. db 확인, 핸드폰 번호 비교
   const isThere = await Token.findOne({ phone: phone });
-  console.log(isThere);
   // 2-1. 저장이 안되어있으면 새로 저장
   if (isThere === null) {
     const token = new Token({
@@ -23,19 +27,24 @@ app.post("/tokens/phone", async (req, res) => {
       phone: phone,
       isAuth: false,
     });
-    console.log("저장안되어있음");
     await token.save();
   }
   // 2-2. 저장이 되어있으면 update
   else {
     await Token.updateOne({ phone: phone }, { $set: { token: number } });
-    console.log("저장되어있음");
   }
-
-  res.send("db 저장완");
+  // 3.0. 핸드폰 번호 유효성 검사
+  const isValid = checkValidationPhone(phone);
+  if (isValid) {
+    // 3. 핸드폰으로 인증번호 전송
+    sendSMS(phone, number);
+    res.send("핸드폰으로 인증번호 전송완료");
+  } else {
+    res.send("유효하지 않은 핸드폰 번호");
+  }
 });
 
-mongoose.connect("mongodb://localhost:27017/myfolder04", () => {
+mongoose.connect(process.env.MONGOOSE_URL, () => {
   console.log("mongodb 접속완료");
 });
 
--- portfolio/day08/phone.js
+++ portfolio/day08/phone.js
@@ -1,8 +1,31 @@
 import crypto from "crypto";
+import coolsms from "coolsms-node-sdk";
+import "dotenv/config";
 
 export function createValidationPhone() {
   let [randomNumber] = crypto.getRandomValues(new Uint32Array(1));
   randomNumber = String(randomNumber).substr(0, 6).padStart(6, "0");
-  console.log(randomNumber);
   return randomNumber;
+}
+
+export function sendSMS(myphone, token) {
+  const SMS_KEY = process.env.SMS_KEY;
+  const SMS_SECRET = process.env.SMS_SECRET;
+  const SMS_SENDER = process.env.SMS_SENDER;
+  const mysms = coolsms.default;
+  const messageService = new mysms(SMS_KEY, SMS_SECRET);
+  messageService.sendOne({
+    to: myphone,
+    from: SMS_SENDER,
+    text: `[코드캠프] 안녕하세요. 인증번호는 ${token} 입니다.`,
+  });
+}
+
+export function checkValidationPhone(myphone) {
+  if (myphone.length !== 10 && myphone.length !== 11) {
+    console.log("에러 발생!!! 핸드폰 번호를 제대로 입력해 주세요!!!");
+    return false;
+  } else {
+    return true;
+  }
 }
