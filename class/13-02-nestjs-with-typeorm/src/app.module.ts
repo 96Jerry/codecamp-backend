17f1d71c21b86c134d58921eb8f2a846cbf6082e
96jerry <junmate12@gmail.com>
2023-02-23 14:06:04 +0900

    nestjs-with-mysql

--- class/13-02-nestjs-with-typeorm/src/app.module.ts
+++ class/13-02-nestjs-with-typeorm/src/app.module.ts
@@ -1,7 +1,9 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardModule } from './apis/boards/boards.module';
+import { Board } from './apis/boards/entities/boards.entity';
 
 @Module({
   imports: [
@@ -9,6 +11,17 @@ import { BoardModule } from './apis/boards/boards.module';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/commons/graphql/schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'dlawns170633!',
+      database: 'myproject03',
+      entities: [Board],
+      synchronize: true,
+      logging: true,
     }),
   ],
 })
