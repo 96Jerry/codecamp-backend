1c384d41efaa3146f3ee5333ee7aa3b462522b69
96jerry <junmate12@gmail.com>
2023-02-24 20:37:41 +0900

    nestjs-typeorm

--- class/13-02-nestjs-with-typeorm/src/apis/boards/boards.resolvers.ts
+++ class/13-02-nestjs-with-typeorm/src/apis/boards/boards.resolvers.ts
@@ -1,12 +1,31 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { BoardService } from './boards.service';
+import { CreateBoardInput } from './dto/createBoard.input';
+import { Board } from './entities/boards.entity';
 
 @Resolver()
 export class BoardResolver {
   constructor(private readonly boardService: BoardService) {}
 
-  @Query(() => String)
-  getHello() {
-    return this.boardService.aaa();
+  // @Query(() => String)
+  // getHello() {
+  //   return this.boardService.aaa();
+  // }
+  @Query(() => [Board])
+  fetchBoards() {
+    return this.boardService.findAll();
+  }
+  @Mutation(() => String)
+  createBoard(
+    @Args({ name: 'writer', nullable: true }) writer: string,
+    @Args('title') title: string,
+    @Args('contents') contents: string,
+    @Args('createBoardInput') createBoardInput: CreateBoardInput,
+  ) {
+    console.log(writer);
+    console.log(title);
+    console.log(contents);
+    console.log(createBoardInput);
+    return this.boardService.create();
   }
 }
