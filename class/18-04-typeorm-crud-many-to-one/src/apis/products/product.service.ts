113738e361038d46a1a64eb5a65402e811fbd706
96jerry <junmate12@gmail.com>
2023-03-03 12:08:36 +0900

    18-04

---
+++ class/18-04-typeorm-crud-many-to-one/src/apis/products/product.service.ts
@@ -0,0 +1,101 @@
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProductSaleslocation } from '../productSaleslocation/entities/productSaleslocation.entity';
+
+@Injectable()
+export class ProductService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+    @InjectRepository(ProductSaleslocation)
+    private readonly productSaleslocationRepository: Repository<ProductSaleslocation>,
+  ) {}
+
+  async findAll() {
+    return await this.productRepository.find({
+      relations: ['productSaleslocation'],
+    });
+  }
+
+  async findOne({ productId }) {
+    return await this.productRepository.findOne({
+      where: { id: productId },
+      relations: ['productSaleslocation'],
+    });
+  }
+
+  //카테고리를 DB에 저장
+  async create({ createProductInput }) {
+    // 1. 상품만 등록하는 경우
+    // const result = await this.productRepository.save({
+    //   ...createProductInput,
+
+    //   // 하나하나 직접 나열하는 방식
+    //   // name: createProductInput.name,
+    //   // description: createProductInput.description,
+    //   // price: createProductInput.price,
+    // });
+    // 2. 상품과 상품거래위치를 같이 등록하는 경우
+    const { productSaleslocation, ...product } = createProductInput;
+    const result = await this.productSaleslocationRepository.save({
+      ...productSaleslocation,
+    });
+    const result2 = await this.productRepository.save({
+      ...product,
+      productSaleslocation: result,
+    });
+    return result2;
+  }
+  async update({ productId, updateProductInput }) {
+    const myproduct = await this.productRepository.findOne({
+      where: { id: productId },
+    });
+
+    const newProduct = {
+      ...myproduct,
+      id: productId,
+      ...updateProductInput,
+    };
+
+    return await this.productRepository.save(newProduct);
+  }
+
+  async checkSoldout({ productId }) {
+    const product = await this.productRepository.findOne({
+      where: { id: productId },
+    });
+
+    if (product.isSoldout) {
+      throw new UnprocessableEntityException('이미 판매 완료된 상품입니다.');
+    }
+
+    // if (product.isSoldout){
+    //   throw new HttpException("이미 판매완료된 상품입니다.", HttpStatus.UNPROCESSABLE_ENTITY)
+    // }
+  }
+
+  async delete({ productId }) {
+    // //1. 실제 삭제
+    // const result = await this.productRepository.delete({ id: productId });
+    // return result.affected ? true : false;
+
+    // //2. 소프트 삭제(직접 구현) - isDeleted
+    // this.productRepository.update({ id: productId }, { isDeleted: true });
+    // // 3. 소프트 삭제(직접 구현) - deletedAt
+    // this.productRepository.update({ id: productId }, { deletedAt: new Date() });
+
+    // // 4. 소프트 삭제(TypeORM 제공)
+    // this.productRepository.softRemove({id: productId}) // 아아디로만 삭제가능
+
+    // 5. 소프트 삭제(TypeORM 제공)
+    const result = await this.productRepository.softDelete({ id: productId });
+    return result.affected ? true : false;
+  }
+}
