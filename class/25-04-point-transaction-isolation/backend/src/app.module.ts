d7b4e09cd23b4fe754d1cfa72801f47d93a805ea
96jerry <junmate12@gmail.com>
2023-03-15 17:50:17 +0900

    serializable

---
+++ class/25-04-point-transaction-isolation/backend/src/app.module.ts
@@ -0,0 +1,39 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './apis/auth/auth.module';
+import { BoardModule } from './apis/boards/boards.module';
+import { PaymentMoudle } from './apis/payment/payment.module';
+import { PointTransactionModule } from './apis/pointTransaction/pointTransaction.module';
+import { ProductCategoryModule } from './apis/productCategory/productCategory.module';
+import { ProductModule } from './apis/products/product.module';
+import { UserModule } from './apis/users/user.module';
+
+@Module({
+  imports: [
+    AuthModule,
+    PointTransactionModule,
+    PaymentMoudle,
+    BoardModule,
+    ProductModule,
+    ProductCategoryModule,
+    UserModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/graphql/schema.gql',
+      context: ({ req, res }) => ({ req, res }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'dlawns170633!',
+      database: 'myproject03',
+      entities: [__dirname + '/apis/**/*.entity.*'],
+      synchronize: true,
+    }),
+  ],
+})
+export class AppModule {}
