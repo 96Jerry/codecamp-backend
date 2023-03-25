7ec5c8125cacfcf50a93f6709c1d8b1b8f7674af
paradise <junmate12@gmail.com>
2023-03-25 22:42:11 +0900

    Create microservice-rest

--- class/32-01-msa-rest/services/resource/src/app.controller.ts
+++ class/32-01-msa-rest/services/resource/src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/aaa')
-  getHello(): string {
-    return this.appService.getHello();
+  // @Get('/aaa')
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+  @MessagePattern({ cmd: 'bbb' })
+  fetchBoards(data) {
+    console.log(data);
+    return 'fetchBoards Success!';
   }
 }
